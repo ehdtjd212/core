1640b6acf1a80f8141462182ec5edc604bec9e2a
ehdtjd212 <dhjung212@gmail.com>
2024-03-13 11:02:26 +0900

    첫번째 커밋

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,42 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImp;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class AppConfig {
+
+    //test
+    @Bean
+    public MemberService memberService(){
+        return new MemberServiceImp(memberRepository());
+    }
+
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImp(
+                memberRepository(),
+                discountPolicy());
+    }
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        System.out.println("true = " + true);
+        return new RateDiscountPolicy();
+        
+    }
+
+}
